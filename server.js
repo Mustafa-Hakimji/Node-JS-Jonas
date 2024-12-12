0d32aab0da12d4c9a1dcc396ad882dce8ee9be92
Mustafa-Hakimji <mustafahussaini757@gmail.com>
2024-12-12 08:38:12 +0530

    Database connected successfully

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
+const DB = process.env.DBURL.replace("<PASSWORD>", process.env.DB_PASSWORD);
+console.log("Final DB URL --> ", DB);
+
+mongoose
+  .connect(DB)
+  .then((con) => console.log("Connected to Data Base Successfully", { con }))
+  .catch((err) => console.log("Fail Database connection ", err));
+
 // console.log(process.env);
 
 const app = require("./app");
+const { dbUrl } = require("./dbConfig");
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
